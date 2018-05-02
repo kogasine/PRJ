7557215102290b5a5b849f7b73d7ee5dbd5e3aee
DESKTOP-TSHV0O5\1234 <id-_-bi@hanmail.net>
2018-05-02 12:16:31 +0900

    asg

---
+++ LED_ONOFF/LED_ONOFF/main.cpp
@@ -0,0 +1,29 @@
+/*
+ * LED_ONOFF.cpp
+ *
+ * Created: 2018-05-02 오전 10:51:57
+ * Author : 1234
+ */ 
+#define  F_CPU 16000000UL
+#include <avr/io.h>
+#include <util/delay.h>
+
+
+int main(void)
+{
+	DDRD = 0xff;
+	PORTD = 0xff;
+	int A=0b00000001;
+    /* Replace with your application code */
+    while (1) 
+    {	PORTD=~0x01;    
+		for(int i=0;i<9;i++){
+			A = A<<1;  
+			PORTD = ~A;
+			_delay_ms(500);
+			
+		}
+		
+    }
+}
+
